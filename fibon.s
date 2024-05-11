e1f0cb14d1a09ef6f0dc0897efba4adf2919be07
SniperMOCs <168505263+SniperMOCs@users.noreply.github.com>
2024-05-12 00:31:14 +0800

    Update fibon.s

--- fibon.s
+++ fibon.s
@@ -1,44 +1,44 @@
 	.file	"fibon.c"
-	.text
+	.text /*Начало текста программы*/
 	.section	.rodata.str1.1,"aMS",@progbits,1
-.LC0:
+.LC0: /*Создание строковой константы*/
 	.string	"Cannot read all the data"
 	.section	.rodata.str1.8,"aMS",@progbits,1
 	.align 8
-.LC1:
+.LC1: /*Создание строковой константы*/
 	.string	"(SUBPROCESS) Fibonacci number %ld is %ld\n"
 	.text
-	.globl	run_fib
+	.globl	run_fib /*Определение глобальной функции*/
 	.type	run_fib, @function
 run_fib:
 .LFB22:
 	.cfi_startproc
-	pushq	%rbx
+	pushq	%rbx /*Сохранение регистра %rbx перед использованием*/
 	.cfi_def_cfa_offset 16
 	.cfi_offset 3, -16
-	subq	$32, %rsp
+	subq	$32, %rsp /*Выделение 32 байт на стеке для временного хранения данных*/
 	.cfi_def_cfa_offset 48
-	movq	%fs:40, %rax
+	movq	%fs:40, %rax /*Копирование значения из таблицы дескрипторов сегментов по смещению 40 в регистр rax*/
 	movq	%rax, 24(%rsp)
 	xorl	%eax, %eax
-	movq	$5, 8(%rsp)
-	movl	4+p(%rip), %edi
-	call	close@PLT
+	movq	$5, 8(%rsp) /*Запись значения 5 в память на 8 байт больше, чем текущий адрес стека*/
+	movl	4+p(%rip), %ediwhile IF /*Загрузка памяти по адресу на 4 байта больше, чем адрес p в регистр edi*/
+	call	close@PLT /*Вызов функции close*/
 	leaq	8(%rsp), %rsi
 	movl	$8, %edx
 	movl	p(%rip), %edi
 	call	read@PLT
 	movq	%rax, %rbx
 	movl	p(%rip), %edi
-	call	close@PLT
-	cmpq	$8, %rbx
-	jne	.L5
+	call	close@PLT /*Вызов close*/
+	cmpq	$8, %rbx /*Сравнение значения 8 и значения в rbx*/
+	jne	.L5 /*Если значения равны, то переход к L5*/
 	movl	$5, %edi
-	call	fibonacci@PLT
+	call	fibonacci@PLT /*Вызов fibonacci*/
 	movq	%rax, %rdx
-	movq	%rax, 16(%rsp)
+	movq	%rax, 16(%rsp) /*Копирование результата работы функции fibonacci*/ 
 	movq	8(%rsp), %rsi
-	leaq	.LC1(%rip), %rdi
+	leaq	.LC1(%rip), %rdi /*Загрузка адреса строковой константы в регистр rdi*/
 	movl	$0, %eax
 	call	printf@PLT
 	movl	o(%rip), %edi
@@ -51,7 +51,7 @@ run_fib:
 	call	close@PLT
 	movq	24(%rsp), %rax
 	subq	%fs:40, %rax
-	jne	.L6
+	jne	.L6 /*Переход к L6*/
 	addq	$32, %rsp
 	.cfi_remember_state
 	.cfi_def_cfa_offset 16
@@ -70,14 +70,14 @@ run_fib:
 .LFE22:
 	.size	run_fib, .-run_fib
 	.section	.rodata.str1.1
-.LC2:
+.LC2: /*Создание строковой константы*/
 	.string	"Hello, World!"
 	.section	.rodata.str1.8
 	.align 8
-.LC3:
+.LC3: /*Создание строковой константы*/
 	.string	"Fibonacci number %ld is %ld. (Taken from child process)\n"
 	.section	.rodata.str1.1
-.LC4:
+.LC4: /*Создание строковой константы*/
 	.string	"A Child process killed."
 	.text
 	.globl	main
@@ -104,13 +104,13 @@ main:
 	call	pipe@PLT
 	call	fork@PLT
 	testl	%eax, %eax
-	jne	.L8
+	jne	.L8 /*Переход к L8*/
 	movl	$0, %eax
 	call	run_fib
 .L9:
 	movq	8(%rsp), %rax
 	subq	%fs:40, %rax
-	jne	.L12
+	jne	.L12 /*Переход к L12*/
 	movl	$0, %eax
 	addq	$24, %rsp
 	.cfi_remember_state
@@ -151,27 +151,27 @@ main:
 	call	waitpid@PLT
 	leaq	.LC4(%rip), %rdi
 	call	puts@PLT
-	jmp	.L9
+	jmp	.L9 /*Переход к L9*/
 .L12:
 	call	__stack_chk_fail@PLT
 	.cfi_endproc
 .LFE23:
 	.size	main, .-main
-	.globl	n
-	.data
+	.globl	n/*Создание переменной n*/
+	.data /**/
 	.align 8
-	.type	n, @object
+	.type	n, @object 
 	.size	n, 8
-n:
+n: 
 	.quad	5
-	.globl	o
+	.globl	o /*Создание переменной o*/
 	.bss
 	.align 8
 	.type	o, @object
 	.size	o, 8
 o:
 	.zero	8
-	.globl	p
+	.globl	p /*Создание переменной p*/
 	.align 8
 	.type	p, @object
 	.size	p, 8
